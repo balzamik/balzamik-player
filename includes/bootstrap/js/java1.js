91b3a21b6c3324e0777cd775bd45efb7c6a26e15
Marin-Balzamik <marin.ortijes@gmail.com>
2014-12-17 10:28:50 +0100

    17.12  v2

--- includes/bootstrap/js/java1.js
+++ includes/bootstrap/js/java1.js
@@ -17,6 +17,7 @@
 		document.body.appendChild(mask);
 		activeNav = "smt-open";
 	} );
+	
 
 	/* hide active menu if mask is clicked */
 	mask.addEventListener( "click", function(){
@@ -27,6 +28,39 @@
 
 	/* hide active menu if close menu button is clicked */
 	[].slice.call(document.querySelectorAll(".close-menu")).forEach(function(el,i){
+		el.addEventListener( "click", function(){
+			classie.remove( body, activeNav );
+			activeNav = "";
+			document.body.removeChild(mask);
+		} );
+	});
+	
+	/* DRUGI OVERLAY */	
+	var body = document.body,
+		mask = document.createElement("div"),
+		toggleSlideTop = document.querySelector( ".toggle-slide-top2" ),
+		slideMenuTop = document.querySelector( ".slide-menu-top2" ),
+		activeNav
+	;
+	mask.className = "mask2";
+
+	/* slide menu top */
+	toggleSlideTop.addEventListener( "click", function(){
+		classie.add( body, "smt-open2" );
+		document.body.appendChild(mask);
+		activeNav = "smt-open2";
+	} );
+	
+
+	/* hide active menu if mask is clicked */
+	mask.addEventListener( "click", function(){
+		classie.remove( body, activeNav );
+		activeNav = "";
+		document.body.removeChild(mask);
+	} );
+
+	/* hide active menu if close menu button is clicked */
+	[].slice.call(document.querySelectorAll(".close-menu2")).forEach(function(el,i){
 		el.addEventListener( "click", function(){
 			classie.remove( body, activeNav );
 			activeNav = "";
